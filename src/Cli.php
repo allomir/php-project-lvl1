4830fb4f295df8118967b5b5e0e11e2b9de14d3a
allomir <alomir@yandex.ru>
2022-08-26 13:23:18 +0300

    new Cli, composer autoload, turning autoload.php brain-games

---
+++ src/Cli.php
@@ -0,0 +1,16 @@
+<?php
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greeting()
+{
+    line("Welcome to the Brain Games!");
+}
+
+function whatIsName()
+{
+    $user = prompt('May I have your name?', 'noname', ' = ');
+    line("Hello, %s!", $user);
+}
